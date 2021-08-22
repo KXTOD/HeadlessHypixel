2eb05c1e94a942f9686989257185ac96de1e7cdf
SimonsBrother <calebthair@outlook.com>
2021-08-22 14:21:29 +0100

    Added random thingy

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 print('Hello world')
 print('How are you')
 print('another test')
@@ -5,6 +7,8 @@ print('another test')
 # Added a comment
 msg = "Hello"
 
-from minecraft.networking.packets import clientbound
+#from minecraft.networking.packets import clientbound
 
-print("Test from PyCharm")
+from random import randint
+for i in range(randint(1,10)):
+    print(randint(0,10))
